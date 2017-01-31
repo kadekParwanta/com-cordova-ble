b2bb2f564666e95cfa73c94032790863bbcae21f
kadekParwanta <kadek.parwanta@gmail.com>
2017-01-31 09:20:12 +0800

    modify build gradle directly

--- hook/beforeBuild.js
+++ hook/beforeBuild.js
@@ -1,4 +1,4 @@
-module.exports = function(ctx) {
+module.exports = function (ctx) {
     // make sure android platform is part of build
     if (ctx.opts.platforms.indexOf('android') < 0) {
         return;
@@ -7,17 +7,31 @@ module.exports = function(ctx) {
         path = ctx.requireCordovaModule('path'),
         deferral = ctx.requireCordovaModule('q').defer();
 
-    var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android/build-extras.gradle');
-    var buildextraspos = path.join(ctx.opts.plugin.dir, 'build-extras.gradle');
+    var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android');
+    var gradleFileLocation = path.join(platformRoot, 'build.gradle');
+    // var buildextraspos = path.join(ctx.opts.plugin.dir, 'build-extras.gradle');
 
-    var reader = fs.createReadStream(buildextraspos);
-    var writer = fs.createWriteStream(platformRoot);
+    // var reader = fs.createReadStream(buildextraspos);
+    // var writer = fs.createWriteStream(platformRoot);
 
-    writer.on('pipe', function(src){
-         console.log('copying ' + buildextraspos + 'to ' + platformRoot);
-        deferral.resolve();
-    })
-    reader.pipe(writer);
+    // writer.on('pipe', function(src){
+    //     console.log('copying ' + buildextraspos + 'to ' + platformRoot);
+    //     deferral.resolve();
+    // })
+    // reader.pipe(writer);
+
+    fs.readFile(gradleFileLocation, 'utf-8', function (err, data) {
+        if (err) throw err;
+
+        var newValue = data.replace('classpath', 'classpath \'io.realm:realm-gradle-plugin:2.1.0\'\nclasspath');
+        newValue = newValue.replace('jcenter()','mavenCentral()\njcenter()')
+
+        fs.writeFile(gradleFileLocation, newValue, 'utf-8', function (err) {
+            if (err) throw err;
+            console.log('build.gradle complete');
+            deferral.resolve();
+        });
+    });
 
     return deferral.promise;
 };
